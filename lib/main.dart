aa8b8236f562cd38e0e94fb6dd4e7af652512dd9
Kumar-Amitt <amitt2015@outlook.com>
2019-10-04 15:13:38 +0530

    Quiz App Layout

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: QuizApp(),
+        backgroundColor: Colors.black,
       ),
     ),
   );
@@ -19,7 +20,60 @@ class _QuizAppState extends State<QuizApp> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Text("Your content here"),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Center(
+              child: Container(
+                child:
+                Text('This is where the question text will go', style: TextStyle(fontSize: 24, color: Colors.white),textAlign: TextAlign.center, ),
+              ),
+            ),
+          ),
+
+          Expanded(
+            flex:2,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                child:Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: FlatButton(
+                        color:Colors.green,
+                        onPressed: (){
+                        },
+                        child:Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text('True', style: TextStyle(fontSize: 24, color: Colors.white),textAlign: TextAlign.center),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: FlatButton(
+                        color:Colors.red,
+                        onPressed: (){
+                        },
+                        child:Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text('False', style: TextStyle(fontSize: 24, color: Colors.white),textAlign: TextAlign.center),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
